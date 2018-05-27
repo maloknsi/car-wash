18735c8c14216e0b50a937d38f496eec72047f20
s.nazarov <s.nazarov@bvblogic.com>
2018-05-27 17:22:52 +0300

    add helper-format text model Order data clear unused code

---
+++ helpers/OrderHelper.php
@@ -0,0 +1,83 @@
+<?php
+namespace app\helpers;
+
+use app\models\Order;
+use app\models\Review;
+
+/**
+ * @package backend\helpers
+ */
+class OrderHelper
+{
+	public static $statuses = [
+		Order::STATUS_BUSY => 'Забронировано',
+		Order::STATUS_CANCEL => 'Отменен',
+		Order::STATUS_SUCCESS => 'Выполнено',
+	];
+	/**
+	 * @param $status
+	 * @return string
+	 */
+	public static function GetStatusText($status)
+	{
+		$result = '';
+		if (isset(self::$statuses[$status])) $result = self::$statuses[$status];
+		return $result;
+	}
+
+	/**
+	 * @param $status
+	 * @return string
+	 */
+	public static function GetStatusTextForClient($status)
+	{
+		$result = self::GetStatusText($status);
+		if ($status == Order::STATUS_BUSY) $result = 'Отменить';
+		return $result;
+	}
+	/**
+	 * @param $timeStart
+	 * @return string
+	 */
+	public static function GetRoundUpTimeStart($timeStart)
+	{
+		$roundUpTimeStart = strtotime('+5 minutes', $timeStart);
+		$roundUpMinutesRest = date('i',$roundUpTimeStart)%5;
+		if ($roundUpMinutesRest != 0) {
+			// если получилось меньше или больше x5 - округляем до х5 в большую сторону
+			$roundUpTimeStart = strtotime('+'.(5 - $roundUpMinutesRest).' minutes',$roundUpTimeStart);
+		}
+		return date('H:i:\0\0', $roundUpTimeStart);
+	}
+
+	/**
+	 * @param $model Order
+	 * @return string
+	 */
+	public static function GetStatusClassForClient($model)
+	{
+		/**
+		 * @return string
+		 **/
+		switch ($model->status) {
+			case Order::STATUS_BUSY:
+				// можно отменить только если время бронирования больше текущего времени (+5минут)
+				if (date('Y-m-d') == $model->date_start && date('H:i', strtotime('+5 minutes')) <=  $model->time_start){
+					$result = "btn-show-confirm-form btn-danger";
+				} else {
+					$result = "btn-default disabled";
+				}
+				break;
+			case Order::STATUS_SUCCESS:
+				$result = "btn-success disabled";
+				break;
+			case Order::STATUS_CANCEL:
+				$result = "btn-default disabled";
+				break;
+			default:
+				$result = "btn-default disabled";
+				break;
+		}
+		return $result;
+	}
+}
--- helpers/ReviewHelper.php
+++ helpers/ReviewHelper.php
@@ -4,7 +4,6 @@ namespace app\helpers;
 use app\models\Review;
 
 /**
- * Class Comments
  * @package backend\helpers
  */
 class ReviewHelper
@@ -23,74 +22,5 @@ class ReviewHelper
 		$result = '';
 		if (isset(self::$statuses[$status])) $result = self::$statuses[$status];
 		return $result;
-	}
-	/**
-	 * @param $status string
-	 * @return string
-	 */
-	public static function GetStatusButton($status)
-	{
-		/**
-		 * @var $comment Review
-		 * @return string
-		 **/
-
-		switch ($status) {
-			case Review::STATUS_MODERATED:
-				$result = '<i class="button-action-moderated fa fa-eye-slash blue" title="На модерации"></i>';
-				break;
-			case Review::STATUS_CONFIRMED:
-				$result = '<i class="button-action-confirmed fa fa-check-circle" title="Одобрен"></i>';
-				break;
-			case Review::STATUS_CANCELED:
-				$result = '<i class="button-action-canceled fa fa-times-circle" title="Отвергнут"></i>';
-				break;
-			default:
-				$result = '';
-				break;
-		}
-		return $result;
-	}
-
-	/**
-	 * @param $status int
-	 * @return string
-	 */
-	public static function GetStatusClass($status)
-	{
-		/**
-		 * @var $comment Review
-		 * @return string
-		 **/
-		switch ($status) {
-			case Review::STATUS_MODERATED:
-				$result = "hidden";
-				break;
-			case Review::STATUS_CONFIRMED:
-				$result = "confirmed";
-				break;
-			case Review::STATUS_CANCELED:
-				$result = "canceled";
-				break;
-			default:
-				$result = "";
-				break;
-		}
-		return $result;
-	}
-
-	/**
-	 * @param $text
-	 * @param int $limit
-	 * @return string
-	 */
-	public static function GetShortText($text, $limit=50)
-	{
-		$result = $text;
-
-		if (mb_strlen($text)>$limit){
-			$result  = mb_substr(htmlentities($text),0,$limit,'UTF-8').'...';
-		}
-		return ($result);
 	}
 }
